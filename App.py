a7d5e5135d9e2a8fc41155d132ad70c8aa7a9c55
ZafraFarhan <145136283+ZafraFarhan@users.noreply.github.com>
2024-04-13 14:43:14 +0530

    Update App.py

--- App.py
+++ App.py
@@ -18,7 +18,7 @@ def run():
     '''
     st.markdown(html_code, unsafe_allow_html=True)
 
-     image_links = [
+    image_links = [
         ("https://example.com/image1.jpg", "https://www.example.com/page1"),
         ("https://example.com/image2.jpg", "https://www.example.com/page2"),
         ("https://example.com/image3.jpg", "https://www.example.com/page3"),

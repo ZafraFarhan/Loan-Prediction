d6b37ad433474f2c5a60f153d7863b84393daf07
ZafraFarhan <145136283+ZafraFarhan@users.noreply.github.com>
2024-04-13 10:56:11 +0530

    Update App.py

--- App.py
+++ App.py
@@ -9,8 +9,8 @@ with open(pickle_file_path, 'rb') as f:
     model = pickle.load(f)
 
 def run():
-    st.markdown("<h1 style='text-align: center; font-size: 80px; font-family: Comic Sans MS;'>FinLoan</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: center; font-size: 15px; font-family: Suez One; font-weight: bold;'>Empowering Your Financial Journey</h2>", unsafe_allow_html=True)
+    #st.markdown("<h1 style='text-align: center; font-size: 80px; font-family: Comic Sans MS;'>FinLoan</h1>", unsafe_allow_html=True)
+    #st.markdown("<h2 style='text-align: center; font-size: 15px; font-family: Suez One; font-weight: bold;'>Empowering Your Financial Journey</h2>", unsafe_allow_html=True)
 
     img1 = Image.open('bank.png')
     img1 = img1.resize((156,145))

63d8ad0e241658d9d1ba69a833a64f92689bb654
ZafraFarhan <145136283+ZafraFarhan@users.noreply.github.com>
2024-04-13 15:02:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -73,7 +73,7 @@ def run():
     <body>
         <ul class="nav-links">
             <li><a href="https://aashafathima1.wixsite.com/finloan">HOME</a></li>
-            <li class="center"><a href="https://aashafathima1.wixsite.com/finloan/about">ABOUT US</a></li>
+            <li class="center"><a href="https://aashafathima1.wixsite.com/finloan/about-us">ABOUT US</a></li>
             <li class="upward"><a href="https://aashafathima1.wixsite.com/finloan/copy-of-about-us">REQUEST LOAN</a></li>
             <li class="forward"><a href="#">APPROVAL STATUS</a></li>
         </ul>

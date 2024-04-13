da581465ee781f21ead2800b12f9e68c3a93643f
ZafraFarhan <145136283+ZafraFarhan@users.noreply.github.com>
2024-04-13 16:10:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -10,14 +10,30 @@ with open(pickle_file_path, 'rb') as f:
 
 def run():
     st.set_page_config(layout="wide")
-    html_code = '''
-    <figure class="image image_resized" style="width:38.38%;" data-ckbox-resource-id="pExlLCMgCXOw">
-        <picture>
-            <source srcset="https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/80.webp 80w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/160.webp 160w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/240.webp 240w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/320.webp 320w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/400.webp 400w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/480.webp 480w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/560.webp 560w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/640.webp 640w,https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/647.webp 647w" sizes="(max-width: 647px) 100vw, 647px" type="image/webp"><img src="https://ckbox.cloud/509c9c1f11e549c80adc/assets/pExlLCMgCXOw/images/647.png" width="647" height="161">
-        </picture>
-    </figure>
-    '''
-    st.markdown(html_code, unsafe_allow_html=True)
+
+    image_path1 = "./H1.png"
+    img2 = Image.open(image_path1)
+
+# Center the image horizontally
+    st.markdown(
+        """
+        <style>
+        .center {
+            display: flex;
+            justify-content: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+# Display the image inside a centered div
+    st.markdown(
+        f"<div class='center'><img src='{image_path1}' width='auto' style='max-width: 100%;'></div>",
+        unsafe_allow_html=True,
+    )
+    
+    
 
     st.markdown("""
     <head>
